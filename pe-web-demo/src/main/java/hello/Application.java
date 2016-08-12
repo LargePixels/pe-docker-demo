a54bea445bd3b675b5def7bd82cf7f6fc06c6db8
John Minchuk <jminchuk@productiveedge.com>
2016-08-12 16:29:10 -0500

    First commit of the pe docker demo.  This is based off the Spring Docker starter project.

---
+++ pe-web-demo/src/main/java/hello/Application.java
@@ -0,0 +1,31 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Productive Edge!";
+	}
+
+	@RequestMapping("/registration")
+	public String registration() {
+
+		//RestTemplate restTemplate = new RestTemplate();
+		//Regsitration registration = restTemplate.getForObject("http://aws-load-balanced-url/get-registration", Registration.class);
+
+		return "Response from registration service";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
